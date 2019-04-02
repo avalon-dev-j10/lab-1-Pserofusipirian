d973cd353647b97b2127c05b078f9a9568f480e8
Кирьяков Вячеслав <>
2019-04-02 19:57:56 +0300

    Сделанная Лабораторная №1

--- src/ru/avalon/java/dev/j10/labs/models/Passport.java
+++ src/ru/avalon/java/dev/j10/labs/models/Passport.java
@@ -1,5 +1,9 @@
 package ru.avalon.java.dev.j10.labs.models;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 /**
  * Представление о паспортных данных человека.
  * <p>
@@ -15,7 +19,7 @@ package ru.avalon.java.dev.j10.labs.models;
  *  <li> орган, выдавший документ.
  * </ol>
  */
-class Passport {
+public class Passport {
 
     /*
      * TODO(Студент): Закончить определение класса.
@@ -37,4 +41,73 @@ class Passport {
      * 5. Обеспечте возможность использования класса за
      *    пределами пакета.
      */
+    
+    //область видимости - в пределах пакета, т.к. нет необходимость использовать где-либо еще
+    String passportNumber;
+    String name;
+    String surname;
+    String middleName;
+    String secondName;
+    Date birthDate;
+    Date issueDate;
+    String issuedBy;
+        
+    Passport(String name, String surname, String middleName, String secondName) {
+        
+        this.name = name;
+        this.surname = surname;
+        
+        if (!middleName.equals("")){
+        this.middleName = middleName;
+        }
+        
+        if (!secondName.equals("")){
+        this.secondName = secondName;
+        }
+    }
+    
+    Passport(String name, String surname, String middleName, String secondName, Date birthDate, 
+            String passportNumber, Date issueDate, String issuedBy) {
+        this(name, surname, middleName, secondName);
+        this.birthDate = birthDate;
+        this.passportNumber = passportNumber;
+        this.issueDate = issueDate;
+        this.issuedBy = issuedBy;        
+    }
+    
+    void SetBirthDate(Date birthDate) {
+        this.birthDate = birthDate;        
+    }
+    
+    void SetPassportData(String passportNumber, Date issueDate, String issuedBy) {
+        this.passportNumber = passportNumber;
+        this.issueDate = issueDate;
+        this.issuedBy = issuedBy;        
+    }
+    
+    String GetName(){
+        return name;
+    }
+    
+    String GetSurname(){
+        return surname;
+    }
+    
+    String GetMiddleName(){
+        return middleName;
+    }
+    
+    String GetSecondName(){
+        return secondName;
+    }
+    
+    Date GetBirthDate(){
+        return birthDate;
+    }
+    
+    String GetPassportData(){
+        DateFormat formatOnlyDate = new SimpleDateFormat("dd.MM.yyyy");
+        
+        return "Паспорт: "+passportNumber+", Выдан: "+issuedBy+", Дата выдачи: "+formatOnlyDate.format(issueDate);
+    }
 }
